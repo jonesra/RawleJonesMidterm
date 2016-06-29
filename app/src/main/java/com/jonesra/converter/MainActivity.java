98f496aaed11201c736066e06bcc92d870906310
jonesra <jonesra1444@gmail.com>
2016-06-29 01:50:10 -0400

    Initial commit

---
+++ app/src/main/java/com/jonesra/converter/MainActivity.java
@@ -0,0 +1,149 @@
+package com.jonesra.converter;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.renderscript.Double2;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        button = (Button) findViewById(R.id.bntConverter);
+        final EditText editText1 = (EditText) findViewById(R.id.edttxtMiles);
+        final EditText editText2 = (EditText) findViewById(R.id.edttxtKilos);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            try {//Try Catch block tracks any errors during the convertion process
+                //Variables Defineded Converter Applcatioin - Rawle Jonews
+                String temp1 = ""; double miles=0; double milesToKilos = 1.60934;
+
+                miles = Double.valueOf(editText1.getText().toString());
+
+                double kilometers = (miles * milesToKilos);
+                temp1 = String.valueOf(kilometers);
+
+                if (!temp1.equals("") && (!temp1.equals(" "))) {
+                    editText2.setText(String.valueOf(temp1));
+                    editText1.setText(String.valueOf(""));
+                } else {
+                    if (temp1 != "0") {
+                        editText2.setText(String.valueOf(temp1));
+                    } else {
+                        editText2.setText(String.valueOf(""));
+                        editText1.setText(String.valueOf("Enter miles"));
+                    }
+                }
+            }  catch (Exception e)
+            {
+                Log.e("error", e.getMessage());
+                editText2.setText(String.valueOf(""));
+                editText1.setText(String.valueOf("Enter miles"));
+
+            };
+        }
+    });
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+            // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.jonesra.converter/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.jonesra.converter/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
